38a7ff26ecc3357c9a75975638ee6b24ff73f948
lsongzhi <lsongzhi@163.com>
2019-02-02 16:39:01 +0800

    unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-#![no_std]
-#![no_main]
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(test), no_main)]
+#![cfg_attr(test, allow(unused_imports))]
 
 use core::panic::PanicInfo;
 
 mod vga_buffer;
 
-
+#[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
@@ -13,6 +14,7 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", info);
