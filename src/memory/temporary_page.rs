97ffef6bf0fdfabe6d87d48f2bded81e75089fff
lsongzhi <lsongzhi@163.com>
2019-02-06 23:27:32 +0800

    temporary_page

--- src/memory/temporary_page.rs
+++ src/memory/temporary_page.rs
@@ -1,14 +1,11 @@
 //! Temporarily map a page
 //! From [Phil Opp's Blog](http://os.phil-opp.com/remap-the-kernel.html)
-
-
-
 use x86_64::{
     VirtAddr,
-    ::structures::paging::{Page, Size4KiB, PhysFrame, PageTableFlags}};
+    ::structures::paging::{Page, Size4KiB, PhysFrame, PageTableFlags, PageTable}};
 use super::ActivePageTable;
 use super::entry::EntryFlags;
-use super::table::{Table, Level1};
+use super::FRAME_ALLOCATOR;
 
 pub struct TemporaryPage {
     page: Page<Size4KiB>,
@@ -27,22 +24,22 @@ impl TemporaryPage {
 
     /// Maps the temporary page to the given frame in the active table.
     /// Returns the start address of the temporary page.
-    pub fn map(&mut self, frame: PhysFrame, flags: PageTableFlags, active_table: &mut ActivePageTable) -> VirtualAddress {
+    pub fn map(&mut self, frame: PhysFrame, flags: PageTableFlags, active_table: &mut ActivePageTable) -> VirtAddr {
         assert!(active_table.translate_page(self.page).is_none(), "temporary page is already mapped");
-        let result = active_table.map_to(self.page, frame, flags);
-        result.flush(active_table);
+        let result = active_table.map_to(self.page, frame, flags, *FRAME_ALLOCATOR).unwrap();
+        result.flush();
         self.page.start_address()
     }
 
     /// Maps the temporary page to the given page table frame in the active
     /// table. Returns a reference to the now mapped table.
-    pub fn map_table_frame(&mut self, frame: Frame, flags: EntryFlags, active_table: &mut ActivePageTable) -> &mut Table<Level1> {
-        unsafe { &mut *(self.map(frame, flags, active_table).get() as *mut Table<Level1>) }
+    pub fn map_table_frame(&mut self, frame: PhysFrame, flags: PageTableFlags, active_table: &mut ActivePageTable) -> &mut PageTable {
+        unsafe { &mut *(self.map(frame, flags, active_table).as_u64() as *mut PageTable) }
     }
 
     /// Unmaps the temporary page in the active table.
     pub fn unmap(&mut self, active_table: &mut ActivePageTable) {
-        let (result, _frame) = active_table.unmap_return(self.page, true);
-        result.flush(active_table);
+        let (_, result) = active_table.unmap(self.page).unwrap();
+        result.flush();
     }
 }
