f899b9b8e1a3596772b4526573671a6bd1ebfdfe
lsongzhi <lsongzhi@163.com>
2019-02-10 16:28:07 +0800

    interrupt pause

--- src/interrupt/mod.rs
+++ src/interrupt/mod.rs
@@ -1,2 +1,9 @@
 pub mod exception;
 pub mod irq;
+
+/// Pause instruction
+/// Safe because it is similar to a NOP, and has no memory effects
+#[inline(always)]
+pub fn pause() {
+    unsafe { asm!("pause" : : : : "intel", "volatile"); }
+}
